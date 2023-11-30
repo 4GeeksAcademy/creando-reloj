2b804fb31596550e5bd26eba28145637892eb9f6
Zurda13 <144481470+Zurda13@users.noreply.github.com>
2023-11-30 19:54:50 +0000

    creacion de reloj 6 digitos

--- src/js/component/contador.js
+++ src/js/component/contador.js
@@ -2,61 +2,57 @@ import React from "react";
 
 function Contador({digitos1, digitos2, digitos3, digitos4, digitos5, digitos6}) {
     return (
-<div className="row">
-
-<div className="col-sm-1 mb-3 mb-sm-0">
-<i class="fa-clock"></i>
-      <div className="card">
-        <div className="card-body bg-dark rounded">
-          <p className="card-text text-white"></p>
+<div className="row justify-content-md-center pt-3 pb-3 bg-secondary" >
+            <div className="col-sm-auto mb-3 mb-sm-0" >
+                <div className="card">
+                <div className="card-body bg-dark rounded" style={{height: "100px"}}>                   
+                    <p className="card-text text-white fs-1"><i className="fa fa-clock text-white"></i> </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-sm-auto mb-3 mb-sm-0 ">
+                <div className="card">
+                <div className="card-body bg-dark rounded" style={{height: "100px"}}>                   
+                    <p className="card-text text-white fs-1">{digitos6}</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-sm-auto mb-3 mb-sm-0">
+                <div className="card">
+                <div className="card-body bg-dark rounded" style={{height: "100px"}}>    
+                    <p className="card-text text-white fs-1">{digitos5}</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-sm-auto mb-3 mb-sm-0">
+                <div className="card">
+                <div className="card-body bg-dark rounded" style={{height: "100px"}}>    
+                    <p className="card-text text-white fs-1">{digitos4}</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-sm-auto mb-3 mb-sm-0">
+                <div className="card">
+                <div className="card-body bg-dark rounded" style={{height: "100px"}}>    
+                    <p className="card-text text-white fs-1">{digitos3}</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-sm-auto mb-3 mb-sm-0">
+                <div className="card">
+                <div className="card-body bg-dark rounded" style={{height: "100px"}}>    
+                    <p className="card-text text-white fs-1">{digitos2}</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-sm-auto mb-3 mb-sm-0">
+                <div className="card">
+                <div className="card-body bg-dark rounded " style={{height: "100px"}}>    
+                    <p className="card-text text-white fs-1">{digitos1}</p>
+                </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    
-    <div className="col-sm-1 mb-3 mb-sm-0">
-      <div className="card">
-        <div className="card-body bg-dark rounded">
-          <p className="card-text text-white">{digitos6}</p>
-        </div>
-      </div>
-    </div>
-    <div className="col-sm-1 mb-3 mb-sm-0">
-      <div className="card">
-        <div className="card-body bg-dark rounded">
-          <p className="card-text text-white">{digitos5}</p>
-        </div>
-      </div>
-    </div>
-    <div className="col-sm-1 mb-3 mb-sm-0">
-      <div className="card">
-        <div className="card-body bg-dark rounded">
-          <p className="card-text text-white">{digitos4}</p>
-        </div>
-      </div>
-    </div>
-    <div className="col-sm-1 mb-3 mb-sm-0">
-      <div className="card">
-        <div className="card-body bg-dark rounded">
-          <p className="card-text text-white">{digitos3}</p>
-        </div>
-      </div>
-    </div>
-    <div className="col-sm-1 mb-3 mb-sm-0">
-      <div className="card">
-        <div className="card-body bg-dark rounded">
-          <p className="card-text text-white">{digitos2}</p>
-        </div>
-      </div>
-    </div>
-    <div className="col-sm-1 mb-3 mb-sm-0">
-      <div className="card">
-        <div className="card-body bg-dark rounded">
-          <p className="card-text text-white">{digitos1}</p>
-        </div>
-      </div>
-    </div>
-
-  </div>
   
    )
 }
